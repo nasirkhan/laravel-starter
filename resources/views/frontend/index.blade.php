b4ce117eb6ec352177705937d144208b53e366cd
nasirkhan <nasir8891@gmail.com>
2017-10-04 14:02:38 +0600

    admin link added

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,11 +8,28 @@
                 <div class="panel-heading">Frontend</div>
 
                 <div class="panel-body">
+                    @auth
+                    <p>
+                        Welcome {{Auth::user()->name}}!
+                    </p>
+                    @endauth
+
                     @guest
-                    Welcome to {{app_name()}}!
-                    <br>
-                    You have to <a href="{{ route('frontend.auth.login') }}">Login</a> first to access dashboard!
+                    <p>
+                        Welcome to {{app_name()}}!
+                        <br>
+                        You have to <a href="{{ route('frontend.auth.login') }}">Login</a> first to access dashboard!
+                    </p>
                     @endguest
+
+                    @can('view_backend')
+
+                    <p>
+                        <a href="{{ route('backend.dashboard') }}" class="btn btn-primary">Visit Admin Dashboard</a>
+                    </p>
+
+
+                    @endcan
                 </div>
             </div>
         </div>
