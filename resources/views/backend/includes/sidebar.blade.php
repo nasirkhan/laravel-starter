f53dbf93fb41c038d314934c744a9769223ad600
nasirkhan <nasir8891@gmail.com>
2017-10-09 12:09:10 +0600

    sidebar moved to new partials

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,65 +1,43 @@
 <div class="sidebar">
     <nav class="sidebar-nav">
         <ul class="nav">
-            <li class="nav-title">
-                {{ __('menus.backend.sidebar.general') }}
-            </li>
-
             <li class="nav-item">
-                <a class="nav-link {{ active_class(Active::checkUriPattern('admin/dashboard')) }}" href="{{ route('admin.dashboard') }}"><i class="icon-speedometer"></i> {{ __('menus.backend.sidebar.dashboard') }}</a>
+                <a class="nav-link" href="index.html"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-primary">NEW</span></a>
             </li>
 
             <li class="nav-title">
-                {{ __('menus.backend.sidebar.system') }}
+                Access Management
             </li>
-
-            @if ($logged_in_user->hasRole('administrator'))
-                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/auth*'), 'open') }}">
-                    <a class="nav-link nav-dropdown-toggle" href="#">
-                        <i class="icon-user"></i> {{ __('menus.backend.access.title') }}
-
-                        @if ($pending_approval > 0)
-                            <span class="tag tag-danger">{{ $pending_approval }}</span>
-                        @endif
-                    </a>
-
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/auth/user*')) }}" href="{{ route('admin.auth.user.index') }}">
-                                {{ __('labels.backend.access.users.management') }}
-
-                                @if ($pending_approval > 0)
-                                    <span class="tag tag-danger">{{ $pending_approval }}</span>
-                                @endif
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/auth/role*')) }}" href="{{ route('admin.auth.role.index') }}">
-                                {{ __('labels.backend.access.roles.management') }}
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            @endif
-
-            <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/log-viewer*'), 'open') }}">
-                <a class="nav-link nav-dropdown-toggle" href="#">
-                    <i class="icon-list"></i> {{ __('menus.backend.log-viewer.main') }}
-                </a>
-
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Menu</a>
                 <ul class="nav-dropdown-items">
                     <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/log-viewer')) }}" href="{{ route('log-viewer::dashboard') }}">
-                            {{ __('menus.backend.log-viewer.dashboard') }}
-                        </a>
+                        <a class="nav-link" href="components-buttons.html"><i class="icon-puzzle"></i> Buttons</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ active_class(Active::checkUriPattern('admin/log-viewer/logs*')) }}" href="{{ route('log-viewer::logs.list') }}">
-                            {{ __('menus.backend.log-viewer.logs') }}
-                        </a>
+                        <a class="nav-link" href="components-social-buttons.html"><i class="icon-puzzle"></i> Social Buttons</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-cards.html"><i class="icon-puzzle"></i> Cards</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-forms.html"><i class="icon-puzzle"></i> Forms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-modals.html"><i class="icon-puzzle"></i> Modals</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-switches.html"><i class="icon-puzzle"></i> Switches</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-tables.html"><i class="icon-puzzle"></i> Tables</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="components-tabs.html"><i class="icon-puzzle"></i> Tabs</a>
                     </li>
                 </ul>
             </li>
+
         </ul>
     </nav>
-</div><!--sidebar-->
+</div>
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -29,49 +29,10 @@
     <!-- / Header Block -->
 
     <div class="app-body">
-        <div class="sidebar">
-            <nav class="sidebar-nav">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-primary">NEW</span></a>
-                    </li>
 
-                    <li class="nav-title">
-                        UI Elements
-                    </li>
-                    <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Menu</a>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-buttons.html"><i class="icon-puzzle"></i> Buttons</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-social-buttons.html"><i class="icon-puzzle"></i> Social Buttons</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-cards.html"><i class="icon-puzzle"></i> Cards</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-forms.html"><i class="icon-puzzle"></i> Forms</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-modals.html"><i class="icon-puzzle"></i> Modals</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-switches.html"><i class="icon-puzzle"></i> Switches</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-tables.html"><i class="icon-puzzle"></i> Tables</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="components-tabs.html"><i class="icon-puzzle"></i> Tabs</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </nav>
-        </div>
+        <!-- Sidebar -->
+        @include('backend.includes.sidebar')
+        <!-- /Sidebar -->
 
         <!-- Main content -->
         <main class="main">
@@ -96,7 +57,7 @@
 
             <div class="container-fluid">
 
-                <div class="animated fadeIn">            
+                <div class="animated fadeIn">
 
                     @yield('content')
 
