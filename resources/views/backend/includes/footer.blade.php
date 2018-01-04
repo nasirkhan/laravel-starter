60cdfa2f17fee7530985e075ece4a58744572002
nasirkhan <nasir8891@gmail.com>
2018-01-04 23:58:10 +1100

    style update

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="app-footer">
     <strong>Copyright &copy; {{ date('Y') }} <a href="https://github.com/nasirkhan/laravel-starter">Laravel Starter</a></strong>
 
-    <span class="float-right text-secondary">Developed with ❤ by <a href="http://nasirkhn.com">Nasir Khan Saikat</a></span>
+    <span class="float-right text-secondary">Developed with ❤ by <a href="http://nasirkhn.com" class="text-secondary">Nasir Khan Saikat</a></span>
 </footer>
