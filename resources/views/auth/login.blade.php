e2ac5299a466862a62c844dac16ae157bc2a2909
nasirkhan <nasir8891@gmail.com>
2018-03-02 12:59:55 +0600

    updatin login form layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,36 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-<div class="container">
+
+
+<main role="main" class="inner cover">
+    <h1 class="cover-heading">Welcome to Latavel Starter</h1>
+
+    <div class="row">
+        <div class="col-12">
+            <form class="">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+</main>
+
+
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -72,5 +101,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
