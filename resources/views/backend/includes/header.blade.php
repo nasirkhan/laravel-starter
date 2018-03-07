bb6062e75e67a2a4b0e71b06d062f052d63078b5
Nasir Khan Saikat <nasir8891@gmail.com>
2018-03-07 15:50:52 +0600

    header new code

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -1,7 +1,11 @@
 <header class="app-header navbar">
-    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">☰</button>
-    <a class="navbar-brand" href="{{ route('frontend.index') }}" target="_blank"></a>
-    <button class="navbar-toggler sidebar-minimizer d-md-down-none" type="button">☰</button>
+    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="{{ route('frontend.index') }}"></a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
     <ul class="nav navbar-nav d-md-down-none">
         <li class="nav-item px-3">
@@ -15,8 +19,20 @@
         </li>
     </ul>
     <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right">
+                <div class="dropdown-header text-center">
+                    <strong>Account</strong>
+                </div>
+                <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> Updates<span class="badge badge-info">42</span></a>
+                <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> Messages<span class="badge badge-success">42</span></a>
+                <a class="dropdown-item" href="#"><i class="fa fa-tasks"></i> Tasks<span class="badge badge-danger">42</span></a>
+                <a class="dropdown-item" href="#"><i class="fa fa-comments"></i> Comments<span class="badge badge-warning">42</span></a>
+            </div>
         </li>
         <li class="nav-item d-md-down-none">
             <a class="nav-link" href="#"><i class="icon-list"></i></a>
@@ -25,25 +41,30 @@
             <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
         </li>
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 <img src="{{ asset('img/avatars/6.jpg') }}" class="img-avatar" alt="{{ Auth::user()->name }}">
-                <span class="d-md-down-none">{{ Auth::user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-header text-center">
-                    <strong>Account</strong>
-                </div>
-                <a class="dropdown-item" href="#"><i class="fas fa-envelope"></i> Messages<span class="badge badge-success">42</span></a>
-
-                <div class="dropdown-header text-center">
                     <strong>Settings</strong>
                 </div>
+                <span class="dropdown-item" href="#">
+                    <i class="fas fa-user"></i> {{ Auth::user()->name }}
+                </span>
+                <span class="dropdown-item" href="#">
+                    <i class="fas fa-at"></i> {{ Auth::user()->email }}
+                </span>
+                <a class="dropdown-item" href="#"><i class="fa fa-wrench"></i> Settings</a>
+                <a class="dropdown-item" href="#"><i class="fa fa-file"></i> Projects<span class="badge badge-primary">42</span></a>
+                <div class="divider"></div>
                 <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}">
-                    <i class="fa fa-lock"></i> Logout
+                    <i class="fas fa-lock"></i> Logout
                 </a>
             </div>
         </li>
     </ul>
-    <button class="navbar-toggler aside-menu-toggler" type="button">☰</button>
+    <button class="navbar-toggler aside-menu-toggler" type="button">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
 </header>
