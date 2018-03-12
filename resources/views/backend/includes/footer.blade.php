5d8d9dca43ace47e67aa75a32c15bde60f3ac1c4
Nasir Khan Saikat <nasir8891@gmail.com>
2018-03-12 16:39:54 +0600

    ❤

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="app-footer text-muted">
     <span>Copyright &copy; {{ date('Y') }} <a href="https://github.com/nasirkhan/laravel-starter">Laravel Starter</a></span>
-    <span class="ml-auto">Developed with ❤ by <a href="http://nasirkhn.com" class="">Nasir Khan Saikat</a></span>
+    <span class="ml-auto">Developed with 🖤 by <a href="http://nasirkhn.com" class="">Nasir Khan Saikat</a></span>
 </footer>
