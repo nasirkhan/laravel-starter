955e57d87e522ea7c785e4aec990bb1f1054ec5a
Nasir Khan Saikat <nasir8891@gmail.com>
2018-03-20 23:14:51 +0600

    cleanup

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -9,9 +9,6 @@
 
     <ul class="nav navbar-nav d-md-down-none">
         <li class="nav-item px-3">
-            <a class="nav-link" href="#">Dashboard</a>
-        </li>
-        <li class="nav-item px-3">
             <a class="nav-link" href="#">Users</a>
         </li>
         <li class="nav-item px-3">
