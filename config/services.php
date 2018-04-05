3df67b49affa1509b7963a81ce34a835595174b9
nasirkhan <nasir8891@gmail.com>
2018-04-05 16:24:07 +0600

    social login enabled

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
         'redirect'      => env('GITHUB_REDIRECT'),
     ],
 
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('FACEBOOK_REDIRECT'),
+    ],
+
 ];
