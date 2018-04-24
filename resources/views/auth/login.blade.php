1ad209151c39a36231b93181777b3102313c547f
Nasir Khan Saikat <nasir8891@gmail.com>
2018-04-24 21:29:46 +0600

    Visual space fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -99,11 +99,11 @@
             <div class="col-12">
                 Login with social profiles <br>
                 <a href="{{route('frontend.auth.social.login', 'facebook')}}" class="btn btn-neutral btn-facebook btn-round">
-                    <i class="fab fa-facebook-square"></i> Facebook
+                    <i class="fab fa-facebook-square"></i>&nbsp; Facebook
                 </a>
 
                 <a href="{{route('frontend.auth.social.login', 'github')}}" class="btn btn-neutral btn-github btn-round">
-                    <i class="fab fa-github-square"></i> Github
+                    <i class="fab fa-github-square"></i>&nbsp; Github
                 </a>
             </div>
         </div>
