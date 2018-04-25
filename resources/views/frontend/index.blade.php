eb8a1a89e11b1d8f158b9c72817531c05d2815d6
Nasir Khan Saikat <nasir8891@gmail.com>
2018-04-25 10:23:08 +0600

    + index text update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,10 +9,12 @@
             <h1 class="title text-center">
                 {{ config('app.name', 'Laravel Starter') }}
             </h1>
-            <!-- <h3 class="category">
-                Register & be a part of the festival!
-            </h3> -->
+            <h3 class="category">
+                An awesome starter project based on Laravel
+            </h3>
+            @can('view_backend')
             <a href="{{ route('backend.dashboard') }}" class="btn btn-primary btn-lg btn-round">Dashboard</a>
+            @endcan
         </div>
     </div>
 </div>
