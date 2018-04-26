14e8fc60e45e1e4e3e64e69d2fc1cfdbf6d4e66f
Nasir Khan Saikat <nasir8891@gmail.com>
2018-04-26 07:48:46 +0000

    Apply fixes from StyleCI

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use App\Notifications\ResetPasswordNotification;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
