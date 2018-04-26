eddf96bf1e10585adfb3a481a82cdc843514448a
Nasir Khan Saikat <nasir8891@gmail.com>
2018-04-26 13:52:25 +0600

    footer layout updated

--- resources/views/auth/footer.blade.php
+++ resources/views/auth/footer.blade.php
@@ -1,48 +1,51 @@
 <footer class="footer">
     <div class="container">
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">
-                        Meet At
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        About Us
-                    </a>
-                </li>
-                @guest
-                <li>
-                    <a href="{{ route('frontend.auth.login') }}">
-                        Login
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('frontend.auth.register') }}">
-                        Register
-                    </a>
-                </li>
-                @else
-                <li>
-                    <a href="#">
-                        {{ Auth::user()->name }}
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('frontend.auth.logout') }}">
-                        Logout
-                    </a>
-                </li>
-                @endguest
-            </ul>
-        </nav>
-        <div class="copyright">
-            &copy;
-            <script>
-                document.write(new Date().getFullYear())
-            </script> Meet At, Developed by
-            <a href="https://nasirkhn.com" target="_blank">Nasir Khan Saikat</a>
+        <div class="row">
+            <div class="col-12 col-md-7">
+                <nav class="float-left">
+                    <ul>
+                        <li>
+                            <a href="/">
+                                Meet At
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                About Us
+                            </a>
+                        </li>
+                        @guest
+                        <li>
+                            <a href="{{ route('frontend.auth.login') }}">
+                                Login
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('frontend.auth.register') }}">
+                                Register
+                            </a>
+                        </li>
+                        @else
+                        <li>
+                            <a href="#">
+                                {{ Auth::user()->name }}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('frontend.auth.logout') }}">
+                                Logout
+                            </a>
+                        </li>
+                        @endguest
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-12 col-md-5">
+                <div class="copyright float-right">
+                    &copy;
+                    <script> document.write(new Date().getFullYear()) </script> Meet At, Developed by
+                    <a href="https://nasirkhn.com" target="_blank">Nasir Khan Saikat</a>
+                </div></div>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
