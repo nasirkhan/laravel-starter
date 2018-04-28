865a76f0a7bb612e6456ef5882920dbd241fbd2b
Nasir Khan Saikat <nasir8891@gmail.com>
2018-04-28 11:06:46 +0600

    social login option separated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -95,18 +95,7 @@
             </form>
         </div>
 
-        <div class="row">
-            <div class="col-12">
-                Login with social profiles <br>
-                <a href="{{route('frontend.auth.social.login', 'facebook')}}" class="btn btn-neutral btn-facebook btn-round">
-                    <i class="fab fa-facebook-square"></i>&nbsp; Facebook
-                </a>
-
-                <a href="{{route('frontend.auth.social.login', 'github')}}" class="btn btn-neutral btn-github btn-round">
-                    <i class="fab fa-github-square"></i>&nbsp; Github
-                </a>
-            </div>
-        </div>
+        @include('auth.social_login_buttons')
     </div>
 </div>
 
