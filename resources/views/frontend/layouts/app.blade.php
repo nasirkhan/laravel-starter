7f78bd6a41308b6f9a671a8fdf1f0726d283548a
Nasir Khan Saikat <nasir8891@gmail.com>
2018-05-02 17:10:13 +0600

    + humans.txt

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <meta name="author" content="Nasir Khan Saikat http://nasirkhn.com">
 
     <link rel="shortcut icon" href="/img/favicon.png">
+    <link type="text/plain" rel="author" href="{{asset('humans.txt')}}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
