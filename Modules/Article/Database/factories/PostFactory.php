6176c1e6f83495be5910d43cbd0c3ec28003e08a
nasirkhan <nasir8891@gmail.com>
2018-07-03 00:47:31 +0600

    image width fix

--- Modules/Article/Database/factories/PostFactory.php
+++ Modules/Article/Database/factories/PostFactory.php
@@ -12,7 +12,7 @@ $factory->define(Modules\Article\Entities\Post::class, function (Faker $faker) {
         'content'           => $faker->paragraph,
         'type'              => $faker->randomElement(['Article', 'Blog', 'News']),
         'is_featured'       => $faker->randomElement(['Yes', 'No']),
-        'featured_image'    => $faker->imageUrl($width = 120, $height = 630),
+        'featured_image'    => $faker->imageUrl($width = 1200, $height = 630),
         'status'            => 1,
         'category_id'       => $faker->numberBetween(1,5),
         'created_at'        => Carbon::now(),
