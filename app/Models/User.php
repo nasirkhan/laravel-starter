9f37f6dd133bfc90cf7bb33e2a23fa3f51434c0a
Nasir Khan Saikat <nasir8891@gmail.com>
2018-07-08 11:25:11 +0600

    passwords will be encrypted from model

--- app/Models/User.php
+++ app/Models/User.php
@@ -101,4 +101,14 @@ class User extends Authenticatable
                 break;
         }
     }
+
+    /**
+     * Set Password and bcrypt before that
+     *
+     * @param String $password Password Text
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
