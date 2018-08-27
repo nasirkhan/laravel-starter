57b8c3134b328ff9c48e644612e2cbe3bfd6c553
Nasir Khan Saikat <nasir8891@gmail.com>
2018-08-27 07:30:27 +0000

    Apply fixes from StyleCI

--- routes/frontend/frontend_routes.php
+++ routes/frontend/frontend_routes.php
@@ -11,12 +11,12 @@ Route::get('home', 'FrontendController@index')->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
     // Route::get('profile', 'FrontendController@profile')->name('profile');
-    Route::get("users/{id}", ['as' => "users.show", 'uses' => "UserController@show"]);
+    Route::get('users/{id}', ['as' => 'users.show', 'uses' => 'UserController@show']);
 
-    Route::get("profile", ['as' => "users.profile", 'uses' => "UserController@profile"]);
-    Route::get("profile/edit", ['as' => "users.profileEdit", 'uses' => "UserController@profileEdit"]);
-    Route::patch("profile/edit", ['as' => "users.profileUpdate", 'uses' => "UserController@profileUpdate"]);
-    Route::delete("users/userProviderDestroy", ['as' => "users.userProviderDestroy", 'uses' => "UserController@userProviderDestroy"]);
-    Route::get("users/profile/changePassword", ['as' => "users.changePassword", 'uses' => "UserController@changePassword"]);
-    Route::patch("users/profile/changePassword", ['as' => "users.changePasswordUpdate", 'uses' => "UserController@changePasswordUpdate"]);
+    Route::get('profile', ['as' => 'users.profile', 'uses' => 'UserController@profile']);
+    Route::get('profile/edit', ['as' => 'users.profileEdit', 'uses' => 'UserController@profileEdit']);
+    Route::patch('profile/edit', ['as' => 'users.profileUpdate', 'uses' => 'UserController@profileUpdate']);
+    Route::delete('users/userProviderDestroy', ['as' => 'users.userProviderDestroy', 'uses' => 'UserController@userProviderDestroy']);
+    Route::get('users/profile/changePassword', ['as' => 'users.changePassword', 'uses' => 'UserController@changePassword']);
+    Route::patch('users/profile/changePassword', ['as' => 'users.changePasswordUpdate', 'uses' => 'UserController@changePasswordUpdate']);
 });
