3e5f93ef83695cff18e034a0699e100f8a4502e2
Nasir Khan Saikat <nasir8891@gmail.com>
2018-09-16 17:20:11 +0600

    +

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -12,45 +12,45 @@ let mix = require('laravel-mix');
  */
 
 // copy assets
-mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js')
-    .copy('resources/assets/js/laravel.js', 'public/js/laravel.js');
+mix.copy("node_modules/jquery/dist/jquery.min.js", "public/js/jquery.min.js")
+    .copy("resources/assets/js/laravel.js", "public/js/laravel.js");
 
 // build frontend css
 mix.styles([
-    'resources/assets/coreui/css/style.min.css',
-    'resources/assets/coreui/css/custom.css'
-], 'public/css/app_frontend.css');
+    "resources/assets/coreui/css/style.min.css",
+    "resources/assets/coreui/css/custom.css"
+], "public/css/app_frontend.css");
 
 // build frontend js
 mix.scripts([
-   'node_modules/jquery/dist/jquery.min.js',
-   'node_modules/popper.js/dist/umd/popper.min.js',
-   'node_modules/bootstrap/dist/js/bootstrap.min.js',
-   'node_modules/pace-progress/pace.min.js',
-   'node_modules/chart.js/dist/Chart.min.js',
-   'resources/assets/js/laravel.js',
-   'resources/assets/coreui/js/app.js'
-], 'public/js/app_frontend.js');
+   "node_modules/jquery/dist/jquery.min.js",
+   "node_modules/popper.js/dist/umd/popper.min.js",
+   "node_modules/bootstrap/dist/js/bootstrap.min.js",
+   "node_modules/pace-progress/pace.min.js",
+   "node_modules/chart.js/dist/Chart.min.js",
+   "resources/assets/js/laravel.js",
+   "resources/assets/coreui/js/app.js"
+], "public/js/app_frontend.js");
 
 
 // build backend css
 mix.styles([
-    'node_modules/@coreui/coreui/dist/css/coreui.min.css',
-    'resources/assets/css/pace.min.css',
-    'resources/assets/css/custom.css'
-], 'public/css/app_backend.css');
+    "node_modules/@coreui/coreui/dist/css/coreui.min.css",
+    "resources/assets/css/pace.min.css",
+    "resources/assets/css/custom.css"
+], "public/css/app_backend.css");
 
 // build backend js
 mix.scripts([
-   'node_modules/jquery/dist/jquery.min.js',
-   'node_modules/popper.js/dist/umd/popper.min.js',
-   'node_modules/bootstrap/dist/js/bootstrap.min.js',
-   'node_modules/pace-progress/pace.min.js',
-   'node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js',
-   'node_modules/chart.js/dist/Chart.min.js',
-   'resources/assets/js/laravel.js',
-   'node_modules/@coreui/coreui/dist/js/coreui.min.js'
-], 'public/js/app_backend.js');
+   "node_modules/jquery/dist/jquery.min.js",
+   "node_modules/popper.js/dist/umd/popper.min.js",
+   "node_modules/bootstrap/dist/js/bootstrap.min.js",
+   "node_modules/pace-progress/pace.min.js",
+   "node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js",
+   "node_modules/chart.js/dist/Chart.min.js",
+   "resources/assets/js/laravel.js",
+   "node_modules/@coreui/coreui/dist/js/coreui.min.js"
+], "public/js/app_backend.js");
 
 
 if (mix.inProduction()) {
