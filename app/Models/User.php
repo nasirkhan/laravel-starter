51640a4d149b090b559820372669b73e7121a691
Nasir Khan Saikat <nasir8891@gmail.com>
2018-09-30 08:49:40 +0000

    Apply fixes from StyleCI

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use App\Notifications\ResetPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
