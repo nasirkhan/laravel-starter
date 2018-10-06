75e98bbeb5ff790802b9b8dc979b95d32ee5690b
Nasir Khan Saikat <nasir8891@gmail.com>
2018-10-06 13:49:50 +0600

    UserCreated Event-Listener to update User Profile

---
+++ app/Listeners/Backend/User/UserCreatedProfileCreate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Listeners\Backend\User;
+
+use App\Events\Backend\User\UserCreated;
+use App\Models\Userprofile;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Log;
+
+class UserCreatedProfileCreate implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserCreated  $event
+     * @return void
+     */
+    public function handle(UserCreated $event)
+    {
+        Log::info('UserCreatedProfileCreate');
+        $user = $event->user;
+
+        $userprofile = new Userprofile;
+        $userprofile->user_id = $user->id;
+        $userprofile->name = $user->name;
+        $userprofile->email = $user->email;
+        $userprofile->avatar = $user->avatar;
+        $userprofile->save();
+
+        //
+    }
+}
