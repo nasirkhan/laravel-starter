faeab44af3db3c58a9afeb1a779b76c57e7f05e3
Nasir Khan Saikat <nasir8891@gmail.com>
2018-10-08 16:43:46 +0600

    +

--- app/helpers.php
+++ app/helpers.php
@@ -136,7 +136,7 @@ if (!function_exists('form_cancel')) {
      *
      * @return mixed
      */
-    function form_cancel($cancel_to, $title, $classes = 'btn btn-danger btn-sm')
+    function form_cancel($cancel_to, $title, $classes = 'btn btn-warning pull-right')
     {
         return resolve(HtmlHelper::class)->formCancel($cancel_to, $title, $classes);
     }
@@ -150,7 +150,7 @@ if (!function_exists('form_submit')) {
      *
      * @return mixed
      */
-    function form_submit($title, $classes = 'btn btn-success btn-sm pull-right')
+    function form_submit($title, $classes = 'btn btn-success')
     {
         return resolve(HtmlHelper::class)->formSubmit($title, $classes);
     }
