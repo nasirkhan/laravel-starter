ae8e5850e1c6815637c183d00a1fe45ed38eefae
Nasir Khan Saikat <nasir8891@gmail.com>
2018-10-23 12:33:05 +0600

    ++

--- Modules/Article/start.php
+++ Modules/Article/start.php
@@ -11,7 +11,7 @@
 |
 */
 
-if (!app()->routesAreCached()) {
+if (! app()->routesAreCached()) {
     require __DIR__.'/Http/routes.php';
 }
 
@@ -22,79 +22,78 @@ if (!app()->routesAreCached()) {
  * *************************************************************************
  */
 \Menu::makeOnce('admin_sidebar', function ($menu) {
-
     // Separator: Module Management
-    $menu->add('Modules', [
+    $all_modules = $menu->add('Modules', [
         'class' => 'nav-title',
     ])
     ->data('order', 2);
 
     // Articles Dropdown
-    $accessControl = $menu->add('<i class="nav-icon fas fa-file-alt"></i> Article', [
+    $articles_menu = $menu->add('<i class="nav-icon fas fa-file-alt"></i> Article', [
         'class' => 'nav-item nav-dropdown',
     ])
     ->data([
-        'order'         => 3,
+        'order' => 3,
         'activematches' => [
             'admin/posts*',
             'admin/categories*',
             'admin/tags*',
         ],
-        'permission'    => ['view_posts', 'view_categories', 'view_tags'],
+        'permission' => ['view_posts', 'view_categories', 'view_tags'],
     ]);
-    $accessControl->link->attr([
+    $articles_menu->link->attr([
         'class' => 'nav-link nav-dropdown-toggle',
-        'href'  => '#',
+        'href' => '#',
     ]);
 
     // Submenu: Posts
-    $accessControl->add('<i class="nav-icon fas fa-file-alt"></i> Posts', [
+    $articles_menu->add('<i class="nav-icon fas fa-file-alt"></i> Posts', [
         'route' => 'backend.posts.index',
         'class' => 'nav-item',
     ])
     ->data([
-        'order'         => 4,
+        'order' => 4,
         'activematches' => 'admin/posts*',
-        'permission'    => ['edit_posts'],
+        'permission' => ['edit_posts'],
     ])
     ->link->attr([
         'class' => 'nav-link',
     ]);
     // Submenu: Categories
-    $accessControl->add('<i class="nav-icon fas fa-sitemap"></i> Categories', [
+    $articles_menu->add('<i class="nav-icon fas fa-sitemap"></i> Categories', [
         'route' => 'backend.categories.index',
         'class' => 'nav-item',
     ])
     ->data([
-        'order'         => 5,
+        'order' => 5,
         'activematches' => 'admin/categories*',
-        'permission'    => ['edit_categories'],
+        'permission' => ['edit_categories'],
     ])
     ->link->attr([
         'class' => 'nav-link',
     ]);
     // Submenu: Tags
-    $accessControl->add('<i class="nav-icon fas fa-tags"></i> Tags', [
+    $articles_menu->add('<i class="nav-icon fas fa-tags"></i> Tags', [
         'route' => 'backend.tags.index',
         'class' => 'nav-item',
     ])
     ->data([
-        'order'         => 6,
+        'order' => 6,
         'activematches' => 'admin/tags*',
-        'permission'    => ['edit_tags'],
+        'permission' => ['edit_tags'],
     ])
     ->link->attr([
         'class' => 'nav-link',
     ]);
     // Submenu: Comments
-    $accessControl->add('<i class="nav-icon fas fa-comments"></i> Comments', [
+    $articles_menu->add('<i class="nav-icon fas fa-comments"></i> Comments', [
         'route' => 'backend.comments.index',
         'class' => 'nav-item',
     ])
     ->data([
-        'order'         => 7,
+        'order' => 7,
         'activematches' => 'admin/comments*',
-        'permission'    => ['edit_comments'],
+        'permission' => ['edit_comments'],
     ])
     ->link->attr([
         'class' => 'nav-link',
