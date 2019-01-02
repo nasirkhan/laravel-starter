627013824ad9f2d45e77ec982cee62c1dfb6f091
Nasir Khan Saikat <nasir8891@gmail.com>
2019-01-02 16:34:27 +0600

    User Profile update/create at Registration

--- app/Listeners/Frontend/User/UserRegisteredProfileCreate.php
+++ app/Listeners/Frontend/User/UserRegisteredProfileCreate.php
@@ -4,6 +4,7 @@ namespace App\Listeners\Frontend\User;
 
 use App\Events\Frontend\User\UserRegistered;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Userprofile;
 
 class UserRegisteredProfileCreate implements ShouldQueue
 {
@@ -27,6 +28,18 @@ class UserRegisteredProfileCreate implements ShouldQueue
     public function handle(UserRegistered $event)
     {
         $user = $event->user;
-        //
+
+        $userprofile = new Userprofile();
+        $userprofile->user_id = $user->id;
+        $userprofile->name = $user->name;
+        $userprofile->email = $user->email;
+        $userprofile->mobile = $user->mobile;
+        $userprofile->gender = $user->gender;
+        $userprofile->date_of_birth = $user->date_of_birth;
+        $userprofile->avatar = $user->avatar;
+        $userprofile->status = ($user->status > 0) ? $user->status : 0;
+        $userprofile->save();
+
+        \Log::debug('UserRegisteredProfileCreate');
     }
 }
