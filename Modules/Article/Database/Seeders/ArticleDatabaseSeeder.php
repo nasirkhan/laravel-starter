c2c883409c5eb5df1a3e7d12988df1fa4111334c
Nasir Khan Saikat <nasir8891@gmail.com>
2019-06-19 10:57:29 +0600

    info added

--- Modules/Article/Database/Seeders/ArticleDatabaseSeeder.php
+++ Modules/Article/Database/Seeders/ArticleDatabaseSeeder.php
@@ -27,24 +27,32 @@ class ArticleDatabaseSeeder extends Seeder
          * ------------------
          */
         DB::table('categories')->truncate();
-        echo "\n Truncate: categories";
+        $this->info("Truncate: categories");
+
         factory(Category::class, 5)->create();
-        echo "\n Insert: categories";
+        $this->info("Insert: categories");
 
         /*
          * Tags Seed
          * ------------------
          */
+
         DB::table('post_tag')->truncate();
+        $this->info("Truncate: post_tag");
+
         DB::table('tags')->truncate();
+        $this->info("Truncate: tags");
+
         factory(Tag::class, 10)->create();
         $tags = Tag::all();
+        $this->info("Insert: tags");
 
         /*
          * Posts Seed
          * ------------------
          */
         DB::table('posts')->truncate();
+        $this->info("Truncate: posts");
 
         // Populate the pivot table
         factory(Post::class, 25)->create()->each(function ($post) use ($tags) {
@@ -52,13 +60,17 @@ class ArticleDatabaseSeeder extends Seeder
                 $tags->random(rand(1, 3))->pluck('id')->toArray()
             );
         });
+        $this->info("Insert: posts");
 
         /*
          * Comment Seed
          * ------------------
          */
         DB::table('comments')->truncate();
+        $this->info("Truncate: comments");
+
         factory(Comment::class, 50)->create();
+        $this->info("Insert: comments");
 
         // Artisan::call('auth:permission', [
         //     'name' => 'posts',
