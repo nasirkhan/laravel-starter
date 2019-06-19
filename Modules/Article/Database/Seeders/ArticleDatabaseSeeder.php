ec612e4b22012a364687e40022e10e2702536e24
Nasir Khan Saikat <nasir8891@gmail.com>
2019-06-19 11:16:34 +0600

    +

--- Modules/Article/Database/Seeders/ArticleDatabaseSeeder.php
+++ Modules/Article/Database/Seeders/ArticleDatabaseSeeder.php
@@ -27,17 +27,10 @@ class ArticleDatabaseSeeder extends Seeder
          * ------------------
          */
         DB::table('categories')->truncate();
-<<<<<<< HEAD
         // $this->info("Truncate: categories");
 
         factory(Category::class, 5)->create();
         // $this->info("Insert: categories");
-=======
-        $this->info('Truncate: categories');
-
-        factory(Category::class, 5)->create();
-        $this->info('Insert: categories');
->>>>>>> 5f3fa259a4678024ed77effb51a26431f0aeda58
 
         /*
          * Tags Seed
@@ -45,7 +38,6 @@ class ArticleDatabaseSeeder extends Seeder
          */
 
         DB::table('post_tag')->truncate();
-<<<<<<< HEAD
         // $this->info("Truncate: post_tag");
 
         DB::table('tags')->truncate();
@@ -54,27 +46,13 @@ class ArticleDatabaseSeeder extends Seeder
         factory(Tag::class, 10)->create();
         $tags = Tag::all();
         // $this->info("Insert: tags");
-=======
-        $this->info('Truncate: post_tag');
-
-        DB::table('tags')->truncate();
-        $this->info('Truncate: tags');
-
-        factory(Tag::class, 10)->create();
-        $tags = Tag::all();
-        $this->info('Insert: tags');
->>>>>>> 5f3fa259a4678024ed77effb51a26431f0aeda58
 
         /*
          * Posts Seed
          * ------------------
          */
         DB::table('posts')->truncate();
-<<<<<<< HEAD
         // $this->info("Truncate: posts");
-=======
-        $this->info('Truncate: posts');
->>>>>>> 5f3fa259a4678024ed77effb51a26431f0aeda58
 
         // Populate the pivot table
         factory(Post::class, 25)->create()->each(function ($post) use ($tags) {
@@ -82,28 +60,17 @@ class ArticleDatabaseSeeder extends Seeder
                 $tags->random(rand(1, 3))->pluck('id')->toArray()
             );
         });
-<<<<<<< HEAD
         // $this->info("Insert: posts");
-=======
-        $this->info('Insert: posts');
->>>>>>> 5f3fa259a4678024ed77effb51a26431f0aeda58
 
         /*
          * Comment Seed
          * ------------------
          */
         DB::table('comments')->truncate();
-<<<<<<< HEAD
         // $this->info("Truncate: comments");
 
         factory(Comment::class, 50)->create();
         // $this->info("Insert: comments");
-=======
-        $this->info('Truncate: comments');
-
-        factory(Comment::class, 50)->create();
-        $this->info('Insert: comments');
->>>>>>> 5f3fa259a4678024ed77effb51a26431f0aeda58
 
         // Artisan::call('auth:permission', [
         //     'name' => 'posts',
