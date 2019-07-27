9b9046f0dac09d403cbf50898a548a6bd9624bd6
Nasir Khan Saikat <nasir8891@gmail.com>
2019-07-27 10:32:30 +0600

    module templete

--- config/modules.php
+++ config/modules.php
@@ -24,7 +24,7 @@ return [
 
     'stubs' => [
         'enabled' => false,
-        'path'    => base_path().'/vendor/nwidart/laravel-modules/src/Commands/stubs',
+        'path'    => base_path().'/Modules/Temp/stubs',
         'files'   => [
             'routes/web'      => 'Routes/web.php',
             'routes/api'      => 'Routes/api.php',
