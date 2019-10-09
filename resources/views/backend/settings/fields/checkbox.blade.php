06478412ee737b36f588a25fa4fbef381925ae41
Nasir Khan Saikat <nasir8891@gmail.com>
2019-10-09 16:57:58 +0600

    cleanup

--- resources/views/backend/settings/fields/checkbox.blade.php
+++ resources/views/backend/settings/fields/checkbox.blade.php
@@ -1,14 +1,3 @@
-<!-- <div class="form-group {{ $errors->has($field['name']) ? ' has-error' : '' }}">
-    <div class="checkbox">
-        <label>
-            <input name="{{ $field['name'] }}" value="1" type="checkbox" @if(old($field['name'], setting($field['name']))) checked="checked" @endif >
-            {{ $field['label'] }}
-        </label>
-
-        @if ($errors->has($field['name'])) <small class="invalid-feedback">{{ $errors->first($field['name']) }}</small> @endif
-    </div>
-</div> -->
-
 <div class="form-group {{ $errors->has($field['name']) ? ' has-error' : '' }}">
     <div class="checkbox">
         <label>
