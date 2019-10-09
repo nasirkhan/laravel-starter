b9b3d10158916d0f5b868d9b87c83f67de8d9e09
Nasir Khan Saikat <nasir8891@gmail.com>
2019-10-09 16:51:53 +0600

    +

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,4 +1,9 @@
 <footer class="app-footer text-muted">
-    <span>Copyright &copy; {{ date('Y') }} <a href="/">{{app_name()}}</a></span>
+    <span>
+        @if(setting('show_copyright'))
+        Copyright &copy; {{ date('Y') }}
+        @endif
+        <a href="/">{{app_name()}}</a>
+    </span>
     <span class="ml-auto" style="">{!! setting('footer_text') !!}</span>
 </footer>
