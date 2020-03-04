a38b54b9ca992b92ebbb864029306f6793501e5f
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-04 11:08:21 +0600

    Newsletter Module Removed

--- Modules/Newsletter/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('newsletter::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('newsletter.name') !!}
-    </p>
-@stop
