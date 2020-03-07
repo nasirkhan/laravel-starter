504845006e85b1dca55330cbfb637871de2dc37b
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-07 12:08:15 +0600

    Fixed Active Menu Check

--- config/laravel-menu/settings.php
+++ config/laravel-menu/settings.php
@@ -4,10 +4,10 @@ return [
     'default' => [
         'auto_activate'    => true,
         'activate_parents' => true,
-        'active_class'     => 'active',
+        'active_class'     => 'open active',
         'restful'          => false,
         'cascade_data'     => true,
         'rest_base'        => '',      // string|array
-        'active_element'   => 'item',  // item|link
+        'active_element'   => 'link',  // item|link
     ],
 ];
