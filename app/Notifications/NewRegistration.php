27af77694a619951d4e79738a40154164f498876
Nasir Khan Saikat <nasir8891@gmail.com>
2020-06-17 02:05:46 +0600

    fix

--- app/Notifications/NewRegistration.php
+++ app/Notifications/NewRegistration.php
@@ -2,9 +2,12 @@
 
 namespace App\Notifications;
 
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 
 class NewRegistration extends Notification
 {
