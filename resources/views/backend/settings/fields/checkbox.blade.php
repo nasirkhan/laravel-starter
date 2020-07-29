3c68d55d0aea95fcf2f149b569325ba866065241
Nasir Khan Saikat <nasir8891@gmail.com>
2020-07-29 11:30:46 +0600

    Required added for all input type

--- resources/views/backend/settings/fields/checkbox.blade.php
+++ resources/views/backend/settings/fields/checkbox.blade.php
@@ -1,10 +1,15 @@
+@php
+$required = (Str::contains($field['rules'], 'required')) ? "required" : "";
+$required_mark = ($required != "") ? '<span class="text-danger"> <strong>*</strong> </span>' : '';
+@endphp
+
 <div class="form-group {{ $errors->has($field['name']) ? ' has-error' : '' }}">
     <div class="checkbox">
         <label>
             <input type='hidden' value='0' name='{{ $field['name'] }}'>
             <input name="{{ $field['name'] }}" value="{{ \Illuminate\Support\Arr::get($field, 'value', '1') }}" type="checkbox" @if(old($field['name'], setting($field['name']))) checked="checked" @endif >
             {{ $field['label'] }}
-        </label>
+        </label> {!! $required_mark !!}
 
         @if ($errors->has($field['name'])) <small class="help-block">{{ $errors->first($field['name']) }}</small> @endif
     </div>
--- resources/views/backend/settings/fields/number.blade.php
+++ resources/views/backend/settings/fields/number.blade.php
@@ -1,11 +1,16 @@
+@php
+$required = (Str::contains($field['rules'], 'required')) ? "required" : "";
+$required_mark = ($required != "") ? '<span class="text-danger"> <strong>*</strong> </span>' : '';
+@endphp
+
 <div class="form-group {{ $errors->has($field['name']) ? ' has-error' : '' }}">
-    <label for="{{ $field['name'] }}"> <strong>{{ $field['label'] }}</strong> ({{ $field['name'] }})</label>
+    <label for="{{ $field['name'] }}"> <strong>{{ $field['label'] }}</strong> ({{ $field['name'] }})</label> {!! $required_mark !!}
     <input type="{{ $field['type'] }}"
            name="{{ $field['name'] }}"
            value="{{ old($field['name'], setting($field['name'])) }}"
            class="form-control {{ Arr::get( $field, 'class') }} {{ $errors->has($field['name']) ? ' is-invalid' : '' }}"
            id="{{ $field['name'] }}"
-           placeholder="{{ $field['label'] }}">
+           placeholder="{{ $field['label'] }}" {{ $required }}>
 
     @if ($errors->has($field['name'])) <small class="invalid-feedback">{{ $errors->first($field['name']) }}</small> @endif
 </div>
