21153bf502f654a0adadf471b096955c317333fa
Nasir Khan Saikat <nasir8891@gmail.com>
2020-07-29 12:40:01 +0600

    Language Files Added and localization option enabled

--- resources/views/backend/includes/errors.blade.php
+++ resources/views/backend/includes/errors.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
 <div class="alert alert-danger" role="alert">
     <p>
-        <i class="fas fa-exclamation-triangle"></i> Please fix the following errors & try again!
+        <i class="fas fa-exclamation-triangle"></i> @lang('Please fix the following errors & try again!')
     </p>
     <ul>
         @foreach ($errors->all() as $error)
--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div>
         <a href="/">{{app_name()}}</a>
         @if(setting('show_copyright'))
-        Copyright &copy; {{ date('Y') }}
+        @lang('Copyright') &copy; {{ date('Y') }}
         @endif
     </div>
     <div class="ml-auto text-muted">{!! setting('footer_text') !!}</div>
