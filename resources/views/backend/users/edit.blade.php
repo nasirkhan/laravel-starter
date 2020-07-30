a48c570154523a868e651fed86842d7e4135d8e7
Nasir Khan Saikat <nasir8891@gmail.com>
2020-07-30 14:00:22 +0600

    Button component enabled

--- resources/views/backend/users/edit.blade.php
+++ resources/views/backend/users/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.app')
 
-@section('title') {{ $module_action }} {{ $module_title }} @endsection
+@section('title') {{ __($module_action) }} {{ $module_title }} @endsection
 
 @section('breadcrumbs')
 <x-backend-breadcrumbs>
@@ -8,7 +8,7 @@
         {{ $module_title }}
     </x-backend-breadcrumb-item>
 
-    <x-backend-breadcrumb-item type="active">{{ $module_action }}</x-backend-breadcrumb-item>
+    <x-backend-breadcrumb-item type="active">{{ __($module_action) }}</x-backend-breadcrumb-item>
 </x-backend-breadcrumbs>
 @endsection
 
@@ -28,21 +28,20 @@
             <!--/.col-->
             <div class="col-4">
                 <div class="btn-toolbar float-right" role="toolbar" aria-label="Toolbar with button groups">
-                    <button onclick="window.history.back();"class="btn btn-warning ml-1" data-toggle="tooltip" title="Return Back"><i class="fas fa-reply"></i></button>
+                    <x-buttons.return-back />
                 </div>
             </div>
-            <!--/.col-->
         </div>
-        <!--/.row-->
         <hr>
-        <div class="row mt-4 mb-4">
+
+        <div class="row mt-4">
             <div class="col">
                 {{ html()->modelForm($user, 'PATCH', route('backend.users.update', $user->id))->class('form-horizontal')->open() }}
 
                     <div class="form-group row">
-                        {{ html()->label(__('labels.backend.users.fields.email'))->class('col-md-2 form-control-label')->for('email') }}
+                        {{ html()->label(__('labels.backend.users.fields.email'))->class('col-sm-2 form-control-label')->for('email') }}
 
-                        <div class="col-md-10">
+                        <div class="col-sm-10">
                             {{ html()->email('email')
                                 ->class('form-control')
                                 ->placeholder(__('labels.backend.users.fields.email'))
@@ -52,25 +51,25 @@
                     </div><!--form-group-->
 
                     <div class="form-group row">
-                        {{ html()->label(__('labels.backend.users.fields.password'))->class('col-md-2 form-control-label')->for('password') }}
+                        {{ html()->label(__('labels.backend.users.fields.password'))->class('col-5 col-sm-2 form-control-label')->for('password') }}
 
-                        <div class="col-md-10">
+                        <div class="col-7 col-sm-10">
                             <a href="{{ route('backend.users.changePassword', $user->id) }}" class="btn btn-outline-primary btn-sm"><i class="fas fa-key"></i> Change password</a>
                         </div>
                     </div><!--form-group-->
 
                     <div class="form-group row">
-                        {{ html()->label('Profile')->class('col-md-2 form-control-label')->for('profile') }}
+                        {{ html()->label('Profile')->class('col-5 col-sm-2 form-control-label')->for('profile') }}
 
-                        <div class="col-md-10">
+                        <div class="col-7 col-sm-10">
                             <a href="{{ route("backend.users.profileEdit", $user->id) }}" class="btn btn-outline-primary btn-sm"><i class="fas fa-user"></i> Update Profile</a>
                         </div>
                     </div><!--form-group-->
 
                     <div class="form-group row">
-                        {{ html()->label(__('labels.backend.users.fields.confirmed'))->class('col-md-2 form-control-label')->for('confirmed') }}
+                        {{ html()->label(__('labels.backend.users.fields.confirmed'))->class('col-5 col-sm-2 form-control-label')->for('confirmed') }}
 
-                        <div class="col-md-10">
+                        <div class="col-7 col-sm-10">
                             @if ($user->email_verified_at == null)
                             <a href="{{route('backend.users.emailConfirmationResend', $user->id)}}" class="btn btn-outline-primary btn-sm " data-toggle="tooltip" title="Send Confirmation Email"><i class="fas fa-envelope"></i> Send Confirmation Email</a>
                             @else
@@ -80,10 +79,10 @@
                     </div><!--form-group-->
 
                     <div class="form-group row">
-                        <div class="col-md-2">
+                        <div class="col-6 col-sm-2">
                             {{ __('labels.backend.users.fields.social') }}
                         </div>
-                        <div class="col-md-10">
+                        <div class="col-6 col-sm-10">
                             <ul class="list-unstyled">
                                 @foreach ($user->providers as $provider)
                                 <li>
@@ -94,66 +93,71 @@
                         </div>
                     </div><!--form-group-->
 
-                    <div class="row">
+                    <div class="form-group row">
+                        {{ html()->label('Abilities')->class('col-sm-2 form-control-label') }}
                         <div class="col">
-                            <div class="card card-accent-primary">
-                                <div class="card-header">
-                                    <strong>Roles</strong>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="card card-accent-primary">
+                                        <div class="card-header">
+                                            @lang('Roles')
+                                        </div>
+                                        <div class="card-body">
+                                            @if ($roles->count())
+                                                @foreach($roles as $role)
+                                                    <div class="card">
+                                                        <div class="card-header">
+                                                            <div class="checkbox">
+                                                                {{ html()->label(html()->checkbox('roles[]', in_array($role->name, $userRoles), $role->name)->id('role-'.$role->id) . "&nbsp;". ucwords($role->name) . "&nbsp;(".$role->name.")")->for('role-'.$role->id) }}
+                                                            </div>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            @if ($role->id != 1)
+                                                                @if ($role->permissions->count())
+                                                                    @foreach ($role->permissions as $permission)
+                                                                        <i class="far fa-check-circle mr-1"></i>{{ $permission->name }}&nbsp;
+                                                                    @endforeach
+                                                                @else
+                                                                    None
+                                                                @endif
+                                                            @else
+                                                                All Permissions
+                                                            @endif
+                                                        </div>
+                                                    </div><!--card-->
+                                                @endforeach
+                                            @endif
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="card-body">
-                                    @if ($roles->count())
-                                        @foreach($roles as $role)
-                                            <div class="card">
-                                                <div class="card-header">
+                                <div class="col-sm-6">
+                                    <div class="card card-accent-info">
+                                        <div class="card-header">
+                                            @lang('Permissions')
+                                        </div>
+                                        <div class="card-body">
+                                            @if ($permissions->count())
+                                                @foreach($permissions as $permission)
                                                     <div class="checkbox">
-                                                        {{ html()->label(html()->checkbox('roles[]', in_array($role->name, $userRoles), $role->name)->id('role-'.$role->id) . ' ' . ucwords($role->name))->for('role-'.$role->id) }}
+                                                        {{ html()->label(html()->checkbox('permissions[]', in_array($permission->name, $userPermissions), $permission->name)->id('permission-'.$permission->id) . ' ' . $permission->name)->for('permission-'.$permission->id) }}
                                                     </div>
-                                                </div>
-                                                <div class="card-body">
-                                                    @if ($role->id != 1)
-                                                        @if ($role->permissions->count())
-                                                            @foreach ($role->permissions as $permission)
-                                                                <i class="fa fa-dot-circle-o"></i> {{ $permission->name }}
-                                                            @endforeach
-                                                        @else
-                                                            None
-                                                        @endif
-                                                    @else
-                                                        All Permissions
-                                                    @endif
-                                                </div>
-                                            </div><!--card-->
-                                        @endforeach
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card card-accent-info">
-                                <div class="card-header">
-                                    <strong>Permissions</strong>
-                                </div>
-                                <div class="card-body">
-                                    @if ($permissions->count())
-                                        @foreach($permissions as $permission)
-                                            <div class="checkbox">
-                                                {{ html()->label(html()->checkbox('permissions[]', in_array($permission->name, $userPermissions), $permission->name)->id('permission-'.$permission->id) . ' ' . $permission->name)->for('permission-'.$permission->id) }}
-                                            </div>
-                                        @endforeach
-                                    @endif
+                                                @endforeach
+                                            @endif
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-sm-4">
                             <div class="form-group">
                                 {{ html()->submit($text = icon('fas fa-save')." Save")->class('btn btn-success') }}
                             </div>
                         </div>
 
-                        <div class="col-8">
+                        <div class="col-sm-8">
                             <div class="float-right">
                                 @if ($$module_name_singular->status != 2 && $$module_name_singular->id != 1)
                                 <a href="{{route('backend.users.block', $$module_name_singular)}}" class="btn btn-danger" data-method="PATCH" data-token="{{csrf_token()}}" data-toggle="tooltip" title="{{__('labels.backend.block')}}" data-confirm="Are you sure?"><i class="fas fa-ban"></i></a>
--- resources/views/components/buttons/edit.blade.php
+++ resources/views/components/buttons/edit.blade.php
@@ -1,19 +1,19 @@
-@props(["route"=>"", "icon"=>"", "title", "small"=>""])
+@props(["route"=>"", "icon"=>"fas fa-wrench", "title", "small"=>"", "class"=>""])
 
 @if($route)
-<a href='{{$route}}' 
-    class='btn btn-primary {{($small=='false')? '' : 'btn-sm'}}' 
-    data-toggle="tooltip" 
+<a href='{{$route}}'
+    class='btn btn-primary {{($small=='true')? 'btn-sm' : ''}} {{$class}}'
+    data-toggle="tooltip"
     title="{{ $title }}">
-    <i class="{{($icon)? $icon :'fas fa-wrench'}}"></i>
+    <i class="{{$icon}}"></i>
     {{ $slot }}
 </a>
 @else
-<button type="submit" 
-    class='btn btn-primary {{($small=='false')? '' : 'btn-sm'}}' 
-    data-toggle="tooltip" 
+<button type="submit"
+    class='btn btn-primary {{($small=='true')? 'btn-sm' : ''}} {{$class}}'
+    data-toggle="tooltip"
     title="{{ $title }}">
-    <i class="{{($icon)? $icon :'fas fa-wrench'}}"></i>
+    <i class="{{$icon}}"></i>
     {{ $slot }}
 </button>
 @endif
