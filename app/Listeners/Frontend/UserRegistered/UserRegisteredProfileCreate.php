b03f59a0c6711be2427e71a9f16f8c7ca17d1ea6
Nasir Khan <nasir8891@gmail.com>
2020-10-07 13:20:14 +0600

    username update

--- app/Listeners/Frontend/UserRegistered/UserRegisteredProfileCreate.php
+++ app/Listeners/Frontend/UserRegistered/UserRegisteredProfileCreate.php
@@ -34,6 +34,7 @@ class UserRegisteredProfileCreate implements ShouldQueue
         $userprofile->name = $user->name;
         $userprofile->first_name = $user->first_name;
         $userprofile->last_name = $user->last_name;
+        $userprofile->username = $user->username;
         $userprofile->email = $user->email;
         $userprofile->mobile = $user->mobile;
         $userprofile->gender = $user->gender;
