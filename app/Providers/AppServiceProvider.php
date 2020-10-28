eab814b47769ced32a9b7c27d45045cb496acbaf
Nasir Khan <nasir8891@gmail.com>
2020-10-28 14:55:31 +0600

    Pagination is Bootstrap based

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
+        
         Blade::component('components.backend-breadcrumbs', 'backendBreadcrumbs');
     }
 }
