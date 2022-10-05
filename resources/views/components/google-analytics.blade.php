b9a357374d5b25b3827a482ef6c8b3eb6a075d2e
Nasir Khan Saikat <nasir8891@gmail.com>
2022-10-06 01:23:51 +0600

    Google analytics updated for Google tag

--- resources/views/components/google-analytics.blade.php
+++ resources/views/components/google-analytics.blade.php
@@ -1,1 +1,13 @@
-{!!setting('google_analytics')!!}
+@if (setting('google_analytics') !== "")
+@php
+$google_analytics = setting('google_analytics');
+@endphp
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $google_analytics }}"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '{{ $google_analytics }}');
+</script>
+@endif
