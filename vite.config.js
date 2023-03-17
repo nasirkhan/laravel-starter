9de7180f9b36b9b93fc7cb66a5c0ced3e8e9b450
Nasir Khan Saikat <nasir8891@gmail.com>
2023-03-17 11:23:41 +0600

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -33,10 +33,5 @@ export default defineConfig({
         //         },
         //     },
         // }),
-    ],
-    resolve: {
-        alias: {
-            '$': 'jQuery',
-        },
-    },
+    ]
 });
