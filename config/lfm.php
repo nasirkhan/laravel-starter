1cd3b39b0ef441aae2fd6a98c07c84f211588f49
Nasir Khan Saikat <nasir8891@gmail.com>
2023-05-14 20:27:04 +0600

    update

--- config/lfm.php
+++ config/lfm.php
@@ -15,7 +15,7 @@ return [
     |--------------------------------------------------------------------------
      */
 
-    'use_package_routes'       => true,
+    'use_package_routes' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -26,18 +26,18 @@ return [
     |
      */
 
-    'allow_private_folder'     => true,
+    'allow_private_folder' => true,
 
     // Flexible way to customize client folders accessibility
     // If you want to customize client folders, publish tag="lfm_handler"
     // Then you can rewrite userField function in App\Handler\ConfigHandler class
     // And set 'user_field' to App\Handler\ConfigHandler::class
     // Ex: The private folder of user will be named as the user id.
-    'private_folder_name'      => UniSharp\LaravelFilemanager\Handlers\ConfigHandler::class,
+    'private_folder_name' => UniSharp\LaravelFilemanager\Handlers\ConfigHandler::class,
 
-    'allow_shared_folder'      => true,
+    'allow_shared_folder' => true,
 
-    'shared_folder_name'       => 'shares',
+    'shared_folder_name' => 'shares',
 
     /*
     |--------------------------------------------------------------------------
@@ -45,15 +45,15 @@ return [
     |--------------------------------------------------------------------------
      */
 
-    'folder_categories'        => [
-        'file'  => [
-            'folder_name'  => 'files',
+    'folder_categories' => [
+        'file' => [
+            'folder_name' => 'files',
             'startup_view' => 'list',
-            'max_size'     => 50000, // size in KB
+            'max_size' => 50000, // size in KB
             'thumb' => true,
             'thumb_width' => 80,
             'thumb_height' => 80,
-            'valid_mime'   => [
+            'valid_mime' => [
                 'image/jpeg',
                 'image/pjpeg',
                 'image/png',
@@ -63,13 +63,13 @@ return [
             ],
         ],
         'image' => [
-            'folder_name'  => 'photos',
+            'folder_name' => 'photos',
             'startup_view' => 'grid',
-            'max_size'     => 50000, // size in KB
+            'max_size' => 50000, // size in KB
             'thumb' => true,
             'thumb_width' => 80,
             'thumb_height' => 80,
-            'valid_mime'   => [
+            'valid_mime' => [
                 'image/jpeg',
                 'image/pjpeg',
                 'image/png',
@@ -94,24 +94,24 @@ return [
     |--------------------------------------------------------------------------
      */
 
-    'disk'                     => 'public-file-manager',
+    'disk' => 'public-file-manager',
 
-    'rename_file'              => true,
+    'rename_file' => true,
 
-    'rename_duplicates'        => true,
+    'rename_duplicates' => true,
 
-    'alphanumeric_filename'    => true,
+    'alphanumeric_filename' => true,
 
-    'alphanumeric_directory'   => true,
+    'alphanumeric_directory' => true,
 
-    'should_validate_size'     => true,
+    'should_validate_size' => true,
 
-    'should_validate_mime'     => true,
+    'should_validate_mime' => true,
 
     // behavior on files with identical name
     // setting it to true cause old file replace with new one
     // setting it to false show `error-file-exist` error and stop upload
-    'over_write_on_duplicate'  => false,
+    'over_write_on_duplicate' => false,
 
     // mimetypes of executables to prevent from uploading
     'disallowed_mimetypes' => ['text/x-php', 'text/html', 'text/plain'],
@@ -128,18 +128,18 @@ return [
     // If true, image thumbnails would be created during upload
     'should_create_thumbnails' => true,
 
-    'thumb_folder_name'        => 'thumbs',
+    'thumb_folder_name' => 'thumbs',
 
     // Create thumbnails automatically only for listed types.
-    'raster_mimetypes'         => [
+    'raster_mimetypes' => [
         'image/jpeg',
         'image/pjpeg',
         'image/png',
     ],
 
-    'thumb_img_width'          => 200, // px
+    'thumb_img_width' => 200, // px
 
-    'thumb_img_height'         => 200, // px
+    'thumb_img_height' => 200, // px
 
     /*
     |--------------------------------------------------------------------------
@@ -147,18 +147,18 @@ return [
     |--------------------------------------------------------------------------
      */
 
-    'file_type_array'          => [
-        'pdf'  => 'Adobe Acrobat',
-        'doc'  => 'Microsoft Word',
+    'file_type_array' => [
+        'pdf' => 'Adobe Acrobat',
+        'doc' => 'Microsoft Word',
         'docx' => 'Microsoft Word',
-        'xls'  => 'Microsoft Excel',
+        'xls' => 'Microsoft Excel',
         'xlsx' => 'Microsoft Excel',
-        'zip'  => 'Archive',
-        'gif'  => 'GIF Image',
-        'jpg'  => 'JPEG Image',
+        'zip' => 'Archive',
+        'gif' => 'GIF Image',
+        'jpg' => 'JPEG Image',
         'jpeg' => 'JPEG Image',
-        'png'  => 'PNG Image',
-        'ppt'  => 'Microsoft PowerPoint',
+        'png' => 'PNG Image',
+        'ppt' => 'Microsoft PowerPoint',
         'pptx' => 'Microsoft PowerPoint',
     ],
 
@@ -173,7 +173,7 @@ return [
     | Please note that the 'upload_max_filesize' & 'post_max_size'
     | directives are not supported.
      */
-    'php_ini_overrides'        => [
+    'php_ini_overrides' => [
         'memory_limit' => '256M',
     ],
 ];
