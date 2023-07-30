a811f4d50883854ab8b30c6d0d70794408cc3795
Nasir Khan Saikat <nasir8891@gmail.com>
2023-07-30 13:49:18 +0600

    Show view update

---
+++ resources/views/components/backend/section-show-table.blade.php
@@ -0,0 +1,40 @@
+@props(["data"=>"", "module_name"])
+<p>
+    @lang("All values of :module_name (Id: :id)", ['module_name'=>ucwords(Str::singular($module_name)), 'id'=>$data->id])
+</p>
+<table class="table table-responsive-sm table-hover table-bordered">
+    <?php
+    $all_columns = $data->getTableColumns();
+    ?>
+    <thead>
+        <tr>
+            <th scope="col">
+                <strong>
+                    @lang('Name')
+                </strong>
+            </th>
+            <th scope="col">
+                <strong>
+                    @lang('Value')
+                </strong>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($all_columns as $column)
+        <tr>
+            <td>
+                <strong>
+                    {{ __(label_case($column->Field)) }}
+                </strong>
+            </td>
+            <td>
+                {!! show_column_value($data, $column) !!}
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{-- Lightbox2 Library --}}
+<x-library.lightbox />
