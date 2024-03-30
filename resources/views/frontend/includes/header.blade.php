a0336d6a03d278adeb9d96b3aeea585de25b5bb0
Nasir Khan Saikat <nasir8891@gmail.com>
2024-03-30 12:26:57 +0600

    +

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -191,7 +191,7 @@
                     class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                     <li>
                         <a class="block rounded bg-blue-700 px-3 py-2 font-semibold text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                            href="#" aria-current="page">Home</a>
+                            href="/" aria-current="page">Home</a>
                     </li>
                     <li>
                         <a class="block rounded px-3 py-2 font-semibold text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
