2a50adf8a008437f55a20e60e2e1281aefdb025e
Nasir Khan Saikat <nasir8891@gmail.com>
2024-04-06 03:13:32 +0600

    test update

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -13,19 +13,29 @@ class RegistrationTest extends TestCase
     {
         $response = $this->get('/register');
 
-        $response->assertStatus(200);
+        if (config('app.user_registration')) {
+            $response->assertStatus(200);
+        } else {
+            $response->assertStatus(404);
+        }
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
+        if (config('app.user_registration')) {
+            $response = $this->post('/register', [
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+            ]);
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('home', absolute: false));
+            $this->assertAuthenticated();
+            $response->assertRedirect(route('home', absolute: false));
+        } else {
+            $response = $this->get('/register');
+            $response->assertStatus(404);
+        }
+
     }
 }
