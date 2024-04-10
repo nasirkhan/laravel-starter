896587063bca469ba0d5c480f466b5e6ed667286
Nasir Khan Saikat <nasir8891@gmail.com>
2024-04-10 06:02:22 +0600

    ++

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -74,15 +74,23 @@
     <section class="bg-gray-50 pb-20 dark:bg-gray-700">
         <div class="grid grid-cols-1 gap-4 p-5 sm:grid-cols-2">
             <div class="rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
-                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/b9ca9cd8-fa7c-43f0-b54f-47e7c4966d9c"
-                    alt="Page preview">
-            </div>
-            <div class="row-span-2 rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
-                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/b067e211-1208-49a6-859b-7a6810e3f3bb"
+                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/1cf5ce5a-f374-4bae-b5a3-69e8d7ff684d"
                     alt="Page preview">
             </div>
             <div class="rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
-                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/413b3c75-4a1f-47e3-8885-bc6bd475213c"
+                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/93341711-60dd-4624-8cd7-82f1c611287d"
+                    alt="Page preview">
+            </div>
+            {{-- <div class="row-span-4 rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
+                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/288f56cb-0cb0-4652-be17-9f65288558bb"
+                    alt="Page preview">
+            </div> --}}
+            <div class="rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
+                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/0f6b8201-6f6a-429f-894b-4e491cc5eba4"
+                    alt="Page preview">
+            </div>
+            <div class="rounded-lg p-3 shadow-lg dark:bg-gray-800 sm:p-10">
+                <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/f8131011-2ecc-4a11-961f-85e02cb8f7a1"
                     alt="Page preview">
             </div>
         </div>
