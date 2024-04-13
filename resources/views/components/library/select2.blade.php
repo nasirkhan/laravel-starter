31a26561b1176c0319dcb9cbb41e23cd51451ba7
Nasir Khan Saikat <nasir8891@gmail.com>
2024-04-13 12:45:46 +0600

    Select2 library update

--- resources/views/components/library/select2.blade.php
+++ resources/views/components/library/select2.blade.php
@@ -1,6 +1,10 @@
 @push('after-styles')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+@if(language_direction() == 'rtl')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
+@else
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+@endif
 @endpush
 
 @push('after-scripts')
@@ -9,7 +13,7 @@
 <script type="module">
     $(document).ready(function() {
         $('.select2').select2({
-            theme: 'bootstrap4',
+            theme: 'bootstrap-5',
             placeholder: '-- Select an option --',
         });
     });
