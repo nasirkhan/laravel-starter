d69e16d50428afa1ce70fe818e157da231686128
Nasir Khan Saikat <nasir8891@gmail.com>
2024-05-19 01:57:39 +0600

    Setting update/store method fixed

--- app/Http/Controllers/Backend/SettingController.php
+++ app/Http/Controllers/Backend/SettingController.php
@@ -68,7 +68,8 @@ class SettingController extends Controller
     public function store(Request $request)
     {
         $rules = Setting::getValidationRules();
-        $data = $request->validate($request, $rules);
+
+        $data = $request->validate($rules);
 
         $validSettings = array_keys($rules);
 
