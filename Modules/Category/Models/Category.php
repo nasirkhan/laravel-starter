540eee6bd2e95cc515f6416317a6617f22a2505f
Nasir Khan Saikat <nasir8891@gmail.com>
2025-10-18 13:10:01 +0600

    Add CategoryStatus enum support and refactor casts and active scope in Category model

--- Modules/Category/Models/Category.php
+++ Modules/Category/Models/Category.php
@@ -3,8 +3,10 @@
 namespace Modules\Category\Models;
 
 use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Category\Enums\CategoryStatus;
 
 class Category extends BaseModel
 {
@@ -14,7 +16,28 @@ class Category extends BaseModel
     protected $table = 'categories';
 
     /**
-     * Caegories has Many posts.
+     * The attributes that should be cast.
+     */
+    protected function casts(): array
+    {
+        return array_merge(parent::casts(), [
+            'status' => CategoryStatus::class,
+        ]);
+    }
+
+    /**
+     * Override the active scope to work with CategoryStatus enum.
+     * 
+     * @param Builder $query
+     * @return void
+     */
+    public function scopeActive(Builder $query): void
+    {
+        $query->where('status', CategoryStatus::Active->value);
+    }
+
+    /**
+     * Categories has Many posts.
      */
     public function posts()
     {
