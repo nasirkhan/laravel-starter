66b8aab7e126b9ba3851cc98b84e952d58e59497
Nasir Khan Saikat <nasir8891@gmail.com>
2025-11-07 02:42:07 +0600

    ++

--- resources/views/components/frontend/header-block.blade.php
+++ resources/views/components/frontend/header-block.blade.php
@@ -1,13 +1,17 @@
-@props(["title" => app_name(), "sub_title" => ""])
+@props(["title" => app_name(), "sub_title" => "", "pre_title" => ""])
 
-<section class="bg-gray-100 py-20 text-gray-600 dark:bg-gray-800 dark:text-gray-400">
+<section class="bg-gray-100 py-20 text-gray-600 dark:bg-slate-900 dark:text-slate-400">
     <div class="container mx-auto flex flex-col items-center justify-center px-5">
         <div class="w-full text-center lg:w-2/3">
-            {!! $sub_title !!}
+            {!! $pre_title !!}
 
-            <h1 class="mb-4 text-3xl font-medium text-gray-800 dark:text-gray-200 sm:text-4xl">
+            <h1 class="mb-4 text-3xl font-medium text-gray-800 sm:text-4xl dark:text-gray-200">
                 {!! $title !!}
             </h1>
+
+            <h2 class="mb-4 text-2xl font-medium text-gray-800 sm:text-2xl dark:text-gray-200">
+                {!! $sub_title !!}
+            </h2>
 
             {!! $slot !!}
 
