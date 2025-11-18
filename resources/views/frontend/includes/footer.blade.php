eef061718f70c3a236083573dd67b706129a42c5
Nasir Khan Saikat <nasir8891@gmail.com>
2025-11-18 20:50:34 +0600

    update footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,9 +1,14 @@
-<footer class="bg-gray-100 p-4 dark:bg-gray-800 sm:p-20" role="contentinfo" aria-label="Site footer">
+<footer class="bg-gray-100 px-4 py-6 sm:p-20 dark:bg-gray-800" role="contentinfo" aria-label="Site footer">
     <div class="mx-auto max-w-5xl text-center">
-        <a class="flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white" href="/" wire:navigate aria-label="Go to homepage">
+        <a
+            class="flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white"
+            href="/"
+            wire:navigate
+            aria-label="Go to homepage"
+        >
             <img class="h-10" src="{{ asset("img/logo-with-text.jpg") }}" alt="{{ app_name() }} Logo" />
         </a>
-        <p class="mx-auto my-6 text-gray-500 dark:text-gray-400 sm:w-1/2">
+        <p class="mx-auto my-6 text-gray-500 sm:w-1/2 dark:text-gray-400">
             {!! setting("meta_description") !!}
         </p>
         <x-frontend.dynamic-menu
