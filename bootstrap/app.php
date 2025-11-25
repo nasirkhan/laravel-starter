7edfa1dfe8923f5b206b798c4985d8864d08cccd
Nasir Khan Saikat <nasir8891@gmail.com>
2025-11-25 21:22:45 +0600

    feat: Integrate Opcodes Log Viewer with custom CoreUI styling and update application middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,14 +7,21 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         // $middleware->web(SetLocale::class);
         $middleware->web(append: [
             SetLocale::class,
+        ]);
+
+        // Register Spatie Permission middleware aliases
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
