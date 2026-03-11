619ecadc06d552b229425040e9da17de1301c1c5
Nasir Khan Saikat <nasir8891@gmail.com>
2026-03-11 22:01:29 +0600

    feat(settings): add footer social profiles visibility toggle
    Add new configuration option to control visibility of footer social
    profiles in the frontend settings.
    
    Improve styling of checkbox and radio input fields by adding proper
    Bootstrap form classes. Enhance radio button layout using flexbox for
    better alignment and spacing between options.

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -16,6 +16,7 @@
             css-class="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white"
         />
 
+        @if (setting("show_footer_social_profiles"))
         <x-cube::social.links
             class="my-6"
             :website="setting('website_url')"
@@ -25,9 +26,13 @@
             :youtube="setting('youtube_url')"
             :whatsapp="setting('whatsapp_url')"
         />
+        @endif
 
+        @if (setting("show_copyright"))
         <x-cube::footer-license license="cc-by-sa" :author="app_name()" :author-url="app_url()" />
+        @endif
 
         <x-cube::footer-credit :text="setting('footer_text')" />
+
     </div>
 </footer>
