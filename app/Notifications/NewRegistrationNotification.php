260384b853a7172f965e1a1235f6ffd5bb56b468
Nasir Khan Saikat <nasir8891@gmail.com>
2026-07-24 00:36:32 +0600

    laravel pint+

--- app/Notifications/NewRegistrationNotification.php
+++ app/Notifications/NewRegistrationNotification.php
@@ -61,9 +61,9 @@ class NewRegistrationNotification extends Notification
 
         return (new MailMessage)
             ->subject('Thank you for registration!')
-            ->line('Thank you for registration at ' . app_name() . '.')
+            ->line('Thank you for registration at '.app_name().'.')
             ->action('Vist Application', url('/'))
-            ->line('We are really happy that you started to use ' . app_name() . '!');
+            ->line('We are really happy that you started to use '.app_name().'!');
     }
 
     /**
@@ -76,7 +76,7 @@ class NewRegistrationNotification extends Notification
     {
         $user = $notifiable;
 
-        $text = 'Registration Completed! | New registration completed for <strong>' . $user->name . '</strong>';
+        $text = 'Registration Completed! | New registration completed for <strong>'.$user->name.'</strong>';
 
         $url_backend = route('backend.users.show', $user->id);
         $url_frontend = route('frontend.users.profile', $user->id);
