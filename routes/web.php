2b6b7e02da85a4fbece5ade94a8ac09093e62c41
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-03 11:15:45 +0600

    Fix formatting inconsistencies in Authorizable trait and web routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 * --------------------------------------------------------------------
 */
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
 
 /*
 *
