271bef8ac69dc2b902a6c8db555af8a3d655175b
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-22 23:53:27 +0600

    Refactor flash message modal to use Bootstrap framework and improve styling

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,23 +1,16 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
     @if ($message['overlay'])
-        <x-cube::modal framework="bootstrap" name="flash-overlay-{{ $loop->index }}">
-            <div class="modal-header">
-                <h5 class="modal-title">{{ $message['title'] }}</h5>
-                <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="{{ __('Close') }}"></button>
-            </div>
-            <div class="modal-body">
-                <p>{!! $message['message'] !!}</p>
-            </div>
-            <div class="modal-footer">
-                <x-cube::button framework="bootstrap" data-coreui-dismiss="modal">{{ __('Close') }}</x-cube::button>
+        <x-cube::modal name="flash-overlay-{{ $loop->index }}" :show="true">
+            <div class="p-6">
+                <h5 id="flash-overlay-{{ $loop->index }}-title" class="text-lg font-medium text-gray-900 dark:text-gray-100">{{ $message['title'] }}</h5>
+                <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">{!! $message['message'] !!}</p>
+                <div class="mt-6 flex justify-end">
+                    <x-cube::button
+                        x-on:click="$dispatch('close-modal', '{{ 'flash-overlay-' . $loop->index }}')"
+                    >{{ __('Close') }}</x-cube::button>
+                </div>
             </div>
         </x-cube::modal>
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var el = document.getElementById('flash-overlay-{{ $loop->index }}');
-                if (el) { new coreui.Modal(el).show(); }
-            });
-        </script>
     @else
         <x-cube::alert :type="$message['level']" :dismissible="$message['important']">
             {!! $message['message'] !!}
